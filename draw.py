18f4d448f268c375c9c086587b52d3e6059045a0
mwalden00 <44304819+mwalden00@users.noreply.github.com>
2020-04-22 13:23:14 -0400

    Add files via upload

---
+++ draw.py
@@ -0,0 +1,373 @@
+from display import *
+from matrix import *
+from gmath import *
+from random import *
+
+def scanline_convert(polygons, n, screen, zbuffer ):
+    '''
+    For each polly:
+        - Find Bottom, Top, Middle vertices
+            * Just find the min, max, and mid y-vals and their respective points
+        - Initial y value is Bottom-y (B_y), final y value is Top-y (T_y)
+        - For each y value to cycle through, draw horizontal lines from x0 to x1
+            * The initial x0 and x1 value will always be Bottom-x (B_x)
+            * We need the change of x over every y value (how much x must change over a given step)
+                ~ This is the same thing as dx/dy (dx), or the inverse slope
+                ~ We also need two seperate dx values for the change in both x0 and x1 (dx0, dx1)
+            * dx1 will change after y reaches Middle-y (M_y)
+                ~ We can just have a second dx1 value (dx1_i, dx1_f)
+            * To find dx0, dx1_i, and dx1_f, use these:
+                ~ dx0 = (x_T - x_B)/(y_T - y_B)
+                ~ dx1_i = (x_M - x_B)/(y_M - y_B)
+                ~ dx1_f = (x_T - x_M)/(y_T - y_M)
+            * Because dx will prob not be an int, our x value cannot be one either
+
+                        ~~~ EZ PZ ~~~
+    '''
+    # find Bottom, Top, Middle
+    color = rand_color()
+    points = [polygons[n],polygons[n+1],polygons[n+2]]
+    points.sort(key=my_key,reverse=True)
+    y_T = points[0][1]; y_M = points[1][1]; y_B = points[2][1]
+    x_T = points[0][0]; x_M = points[1][0]; x_B = points[2][0]
+    z_T = points[0][2]; z_M = points[1][2]; z_B = points[2][2]
+    dx0 = (x_T - x_B)/(y_T - y_B)
+    dz0 = (z_T - z_B)/(y_T - y_B)
+    dx1_i = dx1_f = 0
+    dz1_i = dz1_f = 0
+    if (abs(y_M - y_B) > .001):
+        dx1_i = (x_M - x_B)/(y_M - y_B)
+        dz1_i = (z_M - z_B)/(y_M - y_B)
+    if (abs(y_T - y_M) > .001):
+        dx1_f = (x_T - x_M)/(y_T - y_M)
+        dz1_f = (z_T - z_M)/(y_T - y_M)
+    x0 = x1 = x_B
+    z0 = z1 = z_B
+    y = int(y_B)
+    while y < y_T:
+        draw_line(int(x0), y, z0, int(x1), y, z1, screen, zbuffer, color)
+        x0 += dx0
+        x1 += dx1_i
+        z0 += dz0
+        z1 += dz1_i
+        if y == int(y_M):
+            dx1_i = dx1_f
+            dz1_i = dz1_f
+            dz = z_M
+            x1 = x_M
+        y+=1
+
+
+
+def my_key(l):
+    return l[1]
+
+def scanline(screen, zbuffer, color, x0, x1, y, z0, z1):
+    if(x0 > x1):
+        scanline(screen, zbuffer, color, x1, x0, y, z1, z0)
+        return
+    if(x0 == x1):
+        return
+    x = x0
+    z = z0
+    z_slope = (z1 - z0) / (x1 - x0 + 1)
+    while(x <= x1):
+        plot(screen, zbuffer, color, x, y, z)
+        x += 1
+        z += z_slope
+
+def rand_color():
+    return [randrange(255) for k in range(3)]
+
+
+def add_polygon( polygons, x0, y0, z0, x1, y1, z1, x2, y2, z2 ):
+    add_point(polygons, x0, y0, z0)
+    add_point(polygons, x1, y1, z1)
+    add_point(polygons, x2, y2, z2)
+
+def draw_polygons( polygons, screen, zbuffer, color ):
+    if len(polygons) < 2:
+        print('Need at least 3 points to draw')
+        return
+    point = 0
+    while point < len(polygons) - 2:
+        normal = calculate_normal(polygons, point)[:]
+        #print normal
+        if normal[2] > 0:
+            scanline_convert(polygons, point, screen, zbuffer)
+        point+=3
+
+def add_box( polygons, x, y, z, width, height, depth ):
+    x1 = x + width
+    y1 = y - height
+    z1 = z - depth
+
+    #front
+    add_polygon(polygons, x, y, z, x1, y1, z, x1, y, z)
+    add_polygon(polygons, x, y, z, x, y1, z, x1, y1, z)
+
+    #back
+    add_polygon(polygons, x1, y, z1, x, y1, z1, x, y, z1)
+    add_polygon(polygons, x1, y, z1, x1, y1, z1, x, y1, z1)
+
+    #right side
+    add_polygon(polygons, x1, y, z, x1, y1, z1, x1, y, z1)
+    add_polygon(polygons, x1, y, z, x1, y1, z, x1, y1, z1)
+    #left side
+    add_polygon(polygons, x, y, z1, x, y1, z, x, y, z)
+    add_polygon(polygons, x, y, z1, x, y1, z1, x, y1, z)
+
+    #top
+    add_polygon(polygons, x, y, z1, x1, y, z, x1, y, z1)
+    add_polygon(polygons, x, y, z1, x, y, z, x1, y, z)
+    #bottom
+    add_polygon(polygons, x, y1, z, x1, y1, z1, x1, y1, z)
+    add_polygon(polygons, x, y1, z, x, y1, z1, x1, y1, z1)
+
+def add_sphere(polygons, cx, cy, cz, r, step ):
+    points = generate_sphere(cx, cy, cz, r, step)
+
+    lat_start = 0
+    lat_stop = step
+    longt_start = 0
+    longt_stop = step
+
+    step+= 1
+    for lat in range(lat_start, lat_stop):
+        for longt in range(longt_start, longt_stop):
+
+            p0 = lat * step + longt
+            p1 = p0+1
+            p2 = (p1+step) % (step * (step-1))
+            p3 = (p0+step) % (step * (step-1))
+
+            if longt != step - 2:
+                add_polygon( polygons, points[p0][0],
+                             points[p0][1],
+                             points[p0][2],
+                             points[p1][0],
+                             points[p1][1],
+                             points[p1][2],
+                             points[p2][0],
+                             points[p2][1],
+                             points[p2][2])
+            if longt != 0:
+                add_polygon( polygons, points[p0][0],
+                             points[p0][1],
+                             points[p0][2],
+                             points[p2][0],
+                             points[p2][1],
+                             points[p2][2],
+                             points[p3][0],
+                             points[p3][1],
+                             points[p3][2])
+
+
+def generate_sphere( cx, cy, cz, r, step ):
+    points = []
+
+    rot_start = 0
+    rot_stop = step
+    circ_start = 0
+    circ_stop = step
+
+    for rotation in range(rot_start, rot_stop):
+        rot = rotation/float(step)
+        for circle in range(circ_start, circ_stop+1):
+            circ = circle/float(step)
+
+            x = r * math.cos(math.pi * circ) + cx
+            y = r * math.sin(math.pi * circ) * math.cos(2*math.pi * rot) + cy
+            z = r * math.sin(math.pi * circ) * math.sin(2*math.pi * rot) + cz
+
+            points.append([x, y, z])
+            #print 'rotation: %d\tcircle%d'%(rotation, circle)
+    return points
+
+def add_torus(polygons, cx, cy, cz, r0, r1, step ):
+    points = generate_torus(cx, cy, cz, r0, r1, step)
+
+    lat_start = 0
+    lat_stop = step
+    longt_start = 0
+    longt_stop = step
+
+    for lat in range(lat_start, lat_stop):
+        for longt in range(longt_start, longt_stop):
+
+            p0 = lat * step + longt;
+            if (longt == (step - 1)):
+                p1 = p0 - longt;
+            else:
+                p1 = p0 + 1;
+            p2 = (p1 + step) % (step * step);
+            p3 = (p0 + step) % (step * step);
+
+            add_polygon(polygons,
+                        points[p0][0],
+                        points[p0][1],
+                        points[p0][2],
+                        points[p3][0],
+                        points[p3][1],
+                        points[p3][2],
+                        points[p2][0],
+                        points[p2][1],
+                        points[p2][2] )
+            add_polygon(polygons,
+                        points[p0][0],
+                        points[p0][1],
+                        points[p0][2],
+                        points[p2][0],
+                        points[p2][1],
+                        points[p2][2],
+                        points[p1][0],
+                        points[p1][1],
+                        points[p1][2] )
+
+
+def generate_torus( cx, cy, cz, r0, r1, step ):
+    points = []
+    rot_start = 0
+    rot_stop = step
+    circ_start = 0
+    circ_stop = step
+
+    for rotation in range(rot_start, rot_stop):
+        rot = rotation/float(step)
+        for circle in range(circ_start, circ_stop):
+            circ = circle/float(step)
+
+            x = math.cos(2*math.pi * rot) * (r0 * math.cos(2*math.pi * circ) + r1) + cx;
+            y = r0 * math.sin(2*math.pi * circ) + cy;
+            z = -1*math.sin(2*math.pi * rot) * (r0 * math.cos(2*math.pi * circ) + r1) + cz;
+
+            points.append([x, y, z])
+    return points
+
+
+def add_circle( points, cx, cy, cz, r, step ):
+    x0 = r + cx
+    y0 = cy
+    i = 1
+
+    while i <= step:
+        t = float(i)/step
+        x1 = r * math.cos(2*math.pi * t) + cx;
+        y1 = r * math.sin(2*math.pi * t) + cy;
+
+        add_edge(points, x0, y0, cz, x1, y1, cz)
+        x0 = x1
+        y0 = y1
+        i+= 1
+
+def add_curve( points, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type ):
+
+    xcoefs = generate_curve_coefs(x0, x1, x2, x3, curve_type)[0]
+    ycoefs = generate_curve_coefs(y0, y1, y2, y3, curve_type)[0]
+
+    i = 1
+    while i <= step:
+        t = float(i)/step
+        x = t * (t * (xcoefs[0] * t + xcoefs[1]) + xcoefs[2]) + xcoefs[3]
+        y = t * (t * (ycoefs[0] * t + ycoefs[1]) + ycoefs[2]) + ycoefs[3]
+        #x = xcoefs[0] * t*t*t + xcoefs[1] * t*t + xcoefs[2] * t + xcoefs[3]
+        #y = ycoefs[0] * t*t*t + ycoefs[1] * t*t + ycoefs[2] * t + ycoefs[3]
+
+        add_edge(points, x0, y0, 0, x, y, 0)
+        x0 = x
+        y0 = y
+        i+= 1
+
+
+def draw_lines( matrix, screen, zbuffer, color ):
+    if len(matrix) < 2:
+        print('Need at least 2 points to draw')
+        return
+
+    point = 0
+    while point < len(matrix) - 1:
+        draw_line( int(matrix[point][0]),
+                   int(matrix[point][1]),
+                   matrix[point][2],
+                   int(matrix[point+1][0]),
+                   int(matrix[point+1][1]),
+                   matrix[point+1][2],
+                   screen, zbuffer, color)
+        point+= 2
+
+def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
+    add_point(matrix, x0, y0, z0)
+    add_point(matrix, x1, y1, z1)
+
+def add_point( matrix, x, y, z=0 ):
+    matrix.append( [x, y, z, 1] )
+
+
+
+def draw_line( x0, y0, z0, x1, y1, z1, screen, zbuffer, color ):
+
+    #swap points if going right -> left
+    if x0 > x1:
+        xt = x0
+        yt = y0
+        x0 = x1
+        y0 = y1
+        x1 = xt
+        y1 = yt
+
+    x = x0
+    y = y0
+    A = 2 * (y1 - y0)
+    B = -2 * (x1 - x0)
+    wide = False
+    tall = False
+
+    if ( abs(x1-x0) >= abs(y1 - y0) ): #octants 1/8
+        wide = True
+        loop_start = x
+        loop_end = x1
+        dx_east = dx_northeast = 1
+        dy_east = 0
+        d_east = A
+        if ( A > 0 ): #octant 1
+            d = A + B/2
+            dy_northeast = 1
+            d_northeast = A + B
+        else: #octant 8
+            d = A - B/2
+            dy_northeast = -1
+            d_northeast = A - B
+
+    else: #octants 2/7
+        tall = True
+        dx_east = 0
+        dx_northeast = 1
+        if ( A > 0 ): #octant 2
+            d = A/2 + B
+            dy_east = dy_northeast = 1
+            d_northeast = A + B
+            d_east = B
+            loop_start = y
+            loop_end = y1
+        else: #octant 7
+            d = A/2 - B
+            dy_east = dy_northeast = -1
+            d_northeast = A - B
+            d_east = -1 * B
+            loop_start = y1
+            loop_end = y
+
+    while ( loop_start < loop_end ):
+        plot( screen, zbuffer, color, x, y, 0 )
+        if ( (wide and ((A > 0 and d > 0) or (A < 0 and d < 0))) or
+             (tall and ((A > 0 and d < 0) or (A < 0 and d > 0 )))):
+
+            x+= dx_northeast
+            y+= dy_northeast
+            d+= d_northeast
+        else:
+            x+= dx_east
+            y+= dy_east
+            d+= d_east
+        loop_start+= 1
+    plot( screen, zbuffer, color, x, y, 0 )
